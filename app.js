ae038e2fa00f07c86709c4350be8c4ca86faed7d
Chris Tzitzas <ctzitzas@gmail.com>
2019-01-18 15:31:37 +1100

    Add error handling for routes

--- app.js
+++ app.js
@@ -12,8 +12,25 @@ app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Express server is running on port ${port}`);
-}); 
+});
--- routes.js
+++ routes.js
@@ -62,7 +62,15 @@ router.delete('/:qID/answers/:aID', function (req, res) {
 // POST /questions/:qID/answers/:aID/vote-up
 // POST /questions/:qID/answers/:aID/vote-down
 // vote on a specific answer
-router.post('/:qID/answers/:aID/vote-:dir', function (req, res) {
+router.post('/:qID/answers/:aID/vote-:dir', function (req, res, next) {
+  if (req.params.dir.search(/^(up|down)$/) === -1) {
+    var err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  } else {
+    next(err);
+  }
+}, function (req, res) {
   res.json({
     response: `You sent a POST request to /vote-${req.params.dir}`,
     questionId: req.params.qID,
